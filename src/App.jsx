7f987e3a859e882d2894ab8a2b9cf86071813d5a
Devanshu Sharma <sharma.devanshu089@gmail.com>
2026-07-01 17:39:25 +0530

    Added Simple Demo for Config and Invoke

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import { TitleBar } from "./components/Titlebar/titleBar";
+// Needed to call rust
 import { invoke } from "@tauri-apps/api/core";
+
+
+// This is demo for how to use invoke and contact backend and call rust functions as well as how to get config file
+console.log("Learning this execution");
+let config_struct = invoke("get_config");
+console.log(config_struct);
 export default function App() {
   return <TitleBar />;
 }
