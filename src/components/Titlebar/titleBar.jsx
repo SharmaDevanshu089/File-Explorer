02447b0650738301da945754728909d9d537232f
krishna <kv.250307@gmail.com>
2026-07-01 18:53:17 +0530

    title bar complete images left

--- src/components/Titlebar/titleBar.jsx
+++ src/components/Titlebar/titleBar.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { invoke } from "@tauri-apps/api/core";
 import './title-bar.css';
 
 export function TitleBar() {
@@ -7,9 +8,9 @@ export function TitleBar() {
 
   return (
     <div className="Titlebar-window">
-      <button>-</button>
-      <button onClick={() => setMaximized(!maximized)}>{maximizeIcon}</button>
-      <button>x</button>
+      <button onClick={()=>invoke('do_minimize').catch(console.error)}>-</button>
+      <button onClick={() => {const nextMaximized = !maximized;setMaximized(nextMaximized);invoke(`${nextMaximized?"to_expand":"to_contract"}`).catch(console.error)}}>{maximizeIcon}</button>
+      <button onClick={() => invoke("close_window").catch(console.error)}>x</button>
     </div>
   );
 }
