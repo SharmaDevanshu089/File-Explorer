69a0d3ca74a0fd5451028a957de9f406b611b7b1
Devanshu Sharma <sharma.devanshu089@gmail.com>
2026-07-01 20:57:09 +0530

    Fixed Titlebar Closing Logic

--- src/components/Titlebar/titleBar.jsx
+++ src/components/Titlebar/titleBar.jsx
@@ -1,16 +1,36 @@
 import { useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
+import { getCurrentWindow } from "@tauri-apps/api/window";
 import './title-bar.css';
 
+const appWindow = getCurrentWindow();
 export function TitleBar() {
   const [maximized, setMaximized] = useState(false);
   const maximizeIcon = maximized ? '==' : '=';
 
   return (
     <div className="Titlebar-window">
-      <button onClick={()=>invoke('do_minimize').catch(console.error)}>-</button>
-      <button onClick={() => {const nextMaximized = !maximized;setMaximized(nextMaximized);invoke(`${nextMaximized?"to_expand":"to_contract"}`).catch(console.error)}}>{maximizeIcon}</button>
-      <button onClick={() => invoke("close_window").catch(console.error)}>x</button>
+      <button onClick={do_minimize}>-</button>
+      {/* TODO: Fix this please , call the below function , do not invoke */}
+      <button onClick={() => { const nextMaximized = !maximized; setMaximized(nextMaximized); invoke(`${nextMaximized ? "to_expand" : "to_contract"}`).catch(console.error) }}>{maximizeIcon}</button>
+      <button onClick={close_window}>x</button>
     </div>
   );
+}
+
+
+function close_window() {
+  appWindow.close();
+}
+
+function do_minimize() {
+  appWindow.minimize();
+}
+
+function to_expand() {
+  appWindow.maximize();
+}
+
+function to_contract() {
+  appWindow.unmaximize();
 }
