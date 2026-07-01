8359efb45e208a12cd8945fc1f4efe3dd39881ed
Devanshu Sharma <sharma.devanshu089@gmail.com>
2026-07-01 17:48:01 +0530

    Just Updated Comments for you to read easily

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import { invoke } from "@tauri-apps/api/core";
 
 // This is demo for how to use invoke and contact backend and call rust functions as well as how to get config file
 console.log("Learning this execution");
+// USe await if you need application to stop and wait for function,  in this case you should use await.
+// You should also catch and report all the error this function has errors reporting
 let config_struct = invoke("get_config");
 console.log(config_struct);
 export default function App() {
