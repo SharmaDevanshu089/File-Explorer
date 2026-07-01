964278a99cc975c2e6a4f24573b7678e63c99b89
Devanshu Sharma <sharma.devanshu089@gmail.com>
2026-07-01 16:25:54 +0530

    [Fixed Previous Broken Build]

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,15 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::Manager;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            // Added this for debugging only
+            let app_data_path = app.path().app_data_dir()?;
+
+            println!("App data directory: {:?}", app_data_path);
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
