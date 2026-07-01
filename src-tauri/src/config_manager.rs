592169157635eb26830aec78f36a84aa6d2e9e6a
Devanshu Sharma <sharma.devanshu089@gmail.com>
2026-07-01 16:50:25 +0530

    Added Default Empty Config Location

--- src-tauri/src/config_manager.rs
+++ src-tauri/src/config_manager.rs
@@ -22,9 +22,25 @@ pub fn check_if_config_exists_or_create_one(application_directory: PathBuf) {
     let settings_path = application_directory.join(CONFIG_NAME);
 
     if !settings_path.exists() {
-        if DEBUG {
-            println!("{:?}", settings_path);
+        // Ensureing parent directory exists before writing to it
+        if !application_directory.exists() {
+            fs::create_dir_all(&application_directory)
+                .expect("Failed to create application directory [ERR_CREATE_APP_DIR]");
         }
+
+        // Building Boiler Plate for settings
+        let default_settings: Settings = Settings {
+            initial_directory: PathBuf::from("C:\\"),
+            theme: String::from("dark"),
+        };
+
+        // Serializing the Settings struct into a formatted JSON string
+        let json_data = serde_json::to_string_pretty(&default_settings)
+            .expect("Failed to serialize default settings [ERR_SERIALIZE_CONFIG]");
+
+        fs::write(&settings_path, json_data)
+            .expect("There was Problem Writing Settings Config [ERR_DEFAULT_CONFIG_WRITING]");
     }
+    println!("created new config path : {:#?}", settings_path);
 }
 pub fn load_config() {}
