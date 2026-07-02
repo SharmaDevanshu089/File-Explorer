20a3b6b9e5a6853a0ad648acaa58dd71cd082d32
Devanshu Sharma <sharma.devanshu089@gmail.com>
2026-07-02 10:17:56 +0530

    Added Comments in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,29 @@ import { Sidebar } from "./components/SideBar/Sidebar";
 import { SettingsWindow } from "./components/Windows/SettingsWindow/default";
 import './App.css'
 
+/*
+  IMP FEATURE REQUEST
+
+    I think we need to use Context or Any other Store what you prefer , to store 2 things 
+     CURRENTWINDOW:
+     this will store what current window of application is the user on, like ViewWindow or SettingsWindow,
+     defualt will be ViewWindow , we may or may not add more later.
+
+     CURRENTDIRECTORY:
+     this will store what current directory of application is the user on,
+     we will need this for sidebar to work, as well as for ViewWindow
+     
+*/
+
+
 // This is demo for how to use invoke and contact backend and call rust functions as well as how to get config file
 console.log("Learning this execution");
 // USe await if you need application to stop and wait for function,  in this case you should use await.
 // You should also catch and report all the error this function has errors reporting
 let config_struct = invoke("get_config");
 console.log(config_struct);
+
+
 export default function App() {
   return (
     <div className="app-container">
