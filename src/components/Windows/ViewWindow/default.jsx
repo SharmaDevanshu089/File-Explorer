d3b72a1f7bab366ecbfe2a33da4fc2e1d8f267da
krishna <kv.250307@gmail.com>
2026-07-02 14:41:00 +0530

    top search bar css done

--- src/components/Windows/ViewWindow/default.jsx
+++ src/components/Windows/ViewWindow/default.jsx
@@ -1,14 +1,16 @@
 // create a simple default window export
 import { useDirectoryStore } from "../../../store/useDirectoryStore";
+import './default.css'
 export function ViewWindow() {
     //currentpath and currentDir values will come directly from home page (benefit of zustand)
     const currentDir = useDirectoryStore(state=>state.currentDir)
     const currentPath = useDirectoryStore(state=>state.currentPath)
     return (
         <div className="view-window">
-            <h2>View Window</h2>
-            <p>current Path:{currentDir==''?null:currentDir}{currentPath==''?null:currentPath}</p>   //i just found out this works i can't explain but ya it is short for condition
-            <p>This is the default content area for viewing files.</p>
+            <p className="File-path">{currentDir==''?null:currentDir}{currentPath==''?null:currentPath}</p>
+               {/* //i just found out this works i can't explain but ya it is short for condition */}
+{/* other files will come later */}
+            
         </div>
     );  
 }
