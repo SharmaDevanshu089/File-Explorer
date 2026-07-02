145afcd6053872f5c12b43c5953db19f3f03b6ba
Devanshu Sharma <sharma.devanshu089@gmail.com>
2026-07-02 10:11:55 +0530

    [DEBUG CHANGES] MAde Settings window default for now

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,26 @@ import { TitleBar } from "./components/Titlebar/titleBar";
 import { invoke } from "@tauri-apps/api/core";
 import { TopBar } from "./components/Topbar/topbar";
 import { Sidebar } from "./components/SideBar/Sidebar";
+import { SettingsWindow } from "./components/Windows/SettingsWindow/default";
 import './App.css'
-import { useEffect, useState } from "react";
 
 // This is demo for how to use invoke and contact backend and call rust functions as well as how to get config file
 console.log("Learning this execution");
 // USe await if you need application to stop and wait for function,  in this case you should use await.
 // You should also catch and report all the error this function has errors reporting
+let config_struct = invoke("get_config");
+console.log(config_struct);
 export default function App() {
-  const [currentPath,setCurrentPath]=useState('')
-  const [initialDir,setInitialDir] = useState("")
-  const [completePath,setCompletePath] = useState('')
-  useEffect(
-    ()=>{
- const fetchTheDir = async()=>{
-  try{
-    const response = await invoke("get_config")
-    if(response.initial_directory){setInitialDir(response.initial_directory)} 
-  }catch(err){
-    console.log("Some error on backend that is failed to load config or read yourself:  :",err)
-  }
- };fetchTheDir()
-    },[]
-  )
-  // dont change this useEFfect else it will create infinite renders
-  useEffect(() => {
-    setCompletePath(`${initialDir}${currentPath}`); 
-    console.log(completePath);
-  }, [initialDir, currentPath]);
   return (
     <div className="app-container">
       <div className="main-area-partitioning">
-        <div className="Sidebar-area"><Sidebar setCurrentPath={setCurrentPath}/></div>
-        <div className="Topbar-area"><TitleBar /></div>
+        <div className="Sidebar-area"><Sidebar /></div>
+        {/* TOPBAR SHOULD BE INSIDE APP-WINDOW NOT DIRECTOLY INHERITING BELOW  */}
+        <div className="app-window">
+          <TitleBar />
+          {/* For DEBUG PURPOSES I AM HARDCODING ,WILL CHANGE AFTER APPLICATION PAGE IS DONE */}
+          <SettingsWindow />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/Windows/ViewWindow/default.jsx
@@ -0,0 +1,12 @@
+// create a simple default window export
+
+export function ViewWindow() {
+    return (
+        <div className="view-window">
+            <h2>View Window</h2>
+            <p>This is the default content area for viewing files.</p>
+        </div>
+    );
+}
+
+export default ViewWindow;
