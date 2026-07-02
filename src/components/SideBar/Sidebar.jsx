d888a11a8166b87647853e84ab088b0705a0f35a
Devanshu Sharma <sharma.devanshu089@gmail.com>
2026-07-02 07:47:04 +0530

    Cleaned Sidebar

--- src/components/SideBar/Sidebar.jsx
+++ src/components/SideBar/Sidebar.jsx
@@ -1,15 +1,16 @@
 import { invoke } from "@tauri-apps/api/core"
 import { useEffect } from "react"
-export function Sidebar(){
-  useEffect(()=>{
-    const FetchData = async (name)=>{
-      const path = await invoke("get_library_path",{name}).then(fn => console.log(fn)).finally(console.log("finisshed"))
-    }
-    FetchData("home_dir");
-  },[])
- return(
-  <div>
- 
-  </div>
- )
+export function Sidebar() {
+  // I dont know what are you trying to do here but lione 9 would use Invoke rather than FetchData to get from homedir
+  // useEffect(()=>{
+  //   const FetchData = async (name)=>{
+  //     const path = await invoke("get_library_path",{name}).then(fn => console.log(fn)).finally(console.log("finisshed"))
+  //   }
+  //   FetchData("home_dir");
+  // },[])
+  return (
+    <div>
+
+    </div>
+  )
 }
